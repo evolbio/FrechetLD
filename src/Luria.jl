95767e17d7afa02da3a5c897bee8e3849856cde7
Steve Frank <saf@stevefrank.org>
2022-09-13 07:38:34 -0700

    Preparing for upload to github.

--- src/Luria.jl
+++ src/Luria.jl
@@ -1,7 +1,7 @@
 module Luria
 using Distributions, CSV, DataFrames, StatsBase, Integrals, Optimization, OptimizationOptimJL, 
 		Plots
-export read_salvador, opt_param, plot_cdf
+export read_salvador, opt_param, plot_cdf, plot_diff, plot_all
 
 # file is e.g., "6_7_10"
 function read_salvador(file)
@@ -33,15 +33,54 @@ function opt_param(file, N, u; tol=1e-3)
 	solve(prob, NelderMead(), reltol=tol, callback=callback)
 end
 
+
 function plot_cdf(file, p_fr::Vector{Float64}, Nu::Float64, lb::Float64, ub::Float64)
 	data = read_salvador(file);
 	emp = ecdf(data)
 	plt=plot(x->emp(x),lb,ub,xaxis=:log)
-	#plot!(x->cdf(Frechet(p_fr[1], p_fr[2]), x-p_fr[3]),lb,ub,xaxis=:log)
+	plot!(x->cdf(Frechet(p_fr[1], p_fr[2]), x-p_fr[3]),lb,ub,xaxis=:log)
 	e = MathConstants.e
 	pp = [e/2, e*Nu, Nu*(log(Nu) - 2.35)]
-	#pp = [e/2, e*Nu, Nu*(log(Nu) - 2.35)]
 	plot!(x->cdf(Frechet(pp[1], pp[2]), x-pp[3]),lb,ub,xaxis=:log)
+	display(plt)
+	return plt
+end
+
+function plot_cdf(file, Nu::Float64; lb::Float64=Nu, ub::Float64=Nu*100.,
+			disp=true, xlabel=:none)
+	data = read_salvador(file);
+	emp = ecdf(data)
+	nu = Int(Nu)
+	plt=plot(x->emp(x),lb,ub,xaxis=:log, title="Nu = $nu", xlabel=xlabel,
+				xguidefontsize=16,titlefontsize=14)
+	e = MathConstants.e
+	pp = [e/2, e*Nu, Nu*(log(Nu) - 2.35)]
+	plot!(x->cdf(Frechet(pp[1], pp[2]), x-pp[3]),lb,ub,xaxis=:log,legend=:none)
+	if disp display(plt) end
+	return plt
+end
+
+function plot_all()
+	files = ["7_10_10", "7_9_10", "6_8_10", "6_7_10", "6_6_10"]
+	Nu_list = [1e0, 1e1, 1e2, 1e3, 1e4]
+	n = length(files)
+	plt = Array{Any}(nothing, n)
+	for (f,Nu,i) in zip(files,Nu_list,1:n)
+		plt[i] = plot_cdf(f,Nu;disp=false,
+					xlabel=((i==3) ? "Number of mutants" : ""))
+	end
+	final = plot(plt...,layout=(1,5), size=(1300,300), bottommargins=50Plots.px)
+	display(final)
+	return final
+end
+
+function plot_diff(file, Nu::Float64; lb::Float64=Nu, ub::Float64=Nu*10.)
+	data = read_salvador(file);
+	emp = ecdf(data)
+	e = MathConstants.e
+	pp = [e/2, e*Nu, Nu*(log(Nu) - 2.35)]
+	println("\nloc = ", pp[3], "; scale = ", sqrt(pp[2]))
+	plt=plot(x->emp(x)-cdf(Frechet(pp[1], pp[2]), x-pp[3]),lb,ub)
 	display(plt)
 	return plt
 end
